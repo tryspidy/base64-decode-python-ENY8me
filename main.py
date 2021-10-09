a561c6ed6fc5b9ba46459d752acf8fa65e590328
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 13:03:20 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+>>> import base64
+>>> encoded = base64.b64encode(b'data to be encoded')
+>>> encoded
+b'ZGF0YSB0byBiZSBlbmNvZGVk'
+>>> data = base64.b64decode(encoded)
+>>> data
+b'data to be encoded'
